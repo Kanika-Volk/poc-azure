1a09aa368ff89f4abb5f3fd8be0f519585680886
Kanika-Volk <kanika.goyal@volkswagen.co.in>
2019-10-23 09:52:36 +0530

    Initial commit

---
+++ src/main/java/com/poc/pocazure/PocControllers.java
@@ -0,0 +1,29 @@
+package com.poc.pocazure;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PocControllers {
+
+    @GetMapping("get")
+    ResponseEntity<JSONObject> get(){
+        JSONObject json = new JSONObject();
+        json.put("name","ram");
+        json.put("rollno","1");
+        return new ResponseEntity<JSONObject>(json, HttpStatus.OK);
+    }
+
+    @PostMapping("/post")
+    ResponseEntity<JSONObject> post(@RequestBody JSONObject object){
+        object.put("unique","uniq");
+        return new ResponseEntity<JSONObject>(object, HttpStatus.OK);
+    }
+}
